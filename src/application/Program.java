56506fd39ebd5363bdd79dfb61a057923353550c
Isac644 <isacrafaelcosta644@gmail.com>
2024-05-14 18:35:37 -0300

    First class: position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 
-		System.out.println("First Commit");
+		Position pos = new Position(2, 6);
+		
+		System.out.println(pos);
 
 	}
 
