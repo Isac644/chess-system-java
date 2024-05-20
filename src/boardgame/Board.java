c98b15ddf1873bbf31ae2631c650a92b7642af64
Isac644 <isacrafaelcosta644@gmail.com>
2024-05-20 19:48:35 -0300

    BoardException and defensive programming

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -5,7 +5,10 @@ public class Board {
 	private int columns;
 	private Piece[][] pieces;
 	
-	public Board(int rows, int columns) {
+	public Board(int rows, int columns) throws BoardException{
+		if (rows <= 0 || columns <= 0) {
+			throw new BoardException("Error: Invalid board size");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -14,26 +17,45 @@ public class Board {
 	public int getRows() {
 		return rows;
 	}
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
+	
 	public int getColumns() {
 		return columns;
 	}
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
 	
-	public Piece piece(int row, int column) {
+	public Piece piece(int row, int column)  throws BoardException {
+		if(!positionExists(row, column)) {
+			throw new BoardException("Error: Invalid position");
+		}
 		return pieces[row][column];
 	}
 	
-	public Piece piece(Position position) {
+	public Piece piece(Position position)  throws BoardException {
+		if(!positionExists(position)) {
+			throw new BoardException("Error: Invalid position");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
-	public void placePiece(Piece piece, Position position) {
+	public void placePiece(Piece piece, Position position) throws BoardException{
+		if(thereIsAPiece(position)) {
+			throw new BoardException("Error: Already exists a piece on position");
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+	
+	public boolean positionExists(int row, int column) {
+		return row <= rows && rows >= 0 && column <= columns && column >= 0;
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+	public boolean thereIsAPiece(Position position) throws BoardException {
+		if(!positionExists(position.getRow(), position.getColumn())) {
+			throw new BoardException("Error: Invalid position");
+		}
+		return pieces[position.getRow()][position.getColumn()] != null;
 	}
 }
